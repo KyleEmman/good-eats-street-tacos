f1635829c4ea04511a3af601ed8a5cb1ebb3634d
KyleEmman <kyleemmanuel.ochoa.f@bulsu.edu.ph>
2022-08-28 21:14:32 +0800

    basic responsive website

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h2>Street Tacos</h2>
+        </header>
+        <main>
+            <div class="tacos-container">
+                <div class="functional-icons">
+                    <!--save-->
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-download" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                        <polyline points="7 11 12 16 17 11" />
+                        <line x1="12" y1="4" x2="12" y2="16" />
+                    </svg>
+                    <a href=""><span>save</span></a>
+                    </div>
+                    <!--share-->
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-share" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <circle cx="6" cy="12" r="3" />
+                        <circle cx="18" cy="6" r="3" />
+                        <circle cx="18" cy="18" r="3" />
+                        <line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
+                        <line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
+                    </svg>
+                    <a href=""><span>share</span></a>
+                    </div>
+                    <!--print-->
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
+                        <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+                        <rect x="7" y="13" width="10" height="8" rx="2" />
+                    </svg>
+                    <a href=""><span>print</span></a>
+                    </div>
+                    <!--jump-->
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-symlink" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M4 21v-4a3 3 0 0 1 3 -3h5" />
+                        <path d="M9 17l3 -3l-3 -3" />
+                        <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                        <path d="M5 11v-6a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-9.5" />
+                    </svg>
+                    <a href=""><span>jump</span></a>
+                    </div>
+                </div>
+                <div class="taco-picture">
+                    <img src="images/street-tacos.png" alt="street tacos" class="street-tacos-png">
+                </div>
+                <div class="street-tacos-description">
+                    <p>
+                        Street tacos are delicious, amazing, and oh so mouthwatering! Bite into tender steak, zesty lime flavor with a hint of spice and add on tomatoes, avocado, and onions for a savory bite you are going to love!
+                    </p>
+                    <p>
+                        These street tacos are completely jam-packed with flavor and they are so easy to make. I love how the meat is so tender and juicy and only takes an hour to marinate! If you absolutely love tacos like me, try out these other amazing taco recipes! These taco-stuffed avocados, Baja fish tacos, and ground beef tacos will not disappoint!!
+                    </p>
+                </div>
+            </div>
+            <div class="details-container">
+                <h2>DETAILS</h2>
+                <div>
+                    <p><b>Prep Time:</b> 10 minutes</p>
+                </div>
+                <div>
+                    <p><b>Cook Time:</b> 10 minutes</p>
+                </div>
+                <div>
+                    <p><b>Total Time:</b> 20 minutes</p>
+                </div>
+                <div>
+                    <p><b>Author:</b> Taco Cheesington</p>
+                </div>
+                <div>
+                    <p><b>Servings:</b> 6 Tacos</p>
+                </div>
+            </div>
+            <div class="ingredients-directions-container">
+                <div class="ingredients">
+                    <h3>Ingredients</h3>
+                    <ul>
+                        <li>Flank steak</li>
+                        <li>Soy Sauce</li>
+                        <li>Worcestershire Sauce</li>
+                        <li>Lime</li>
+                        <li>Minced Garlic</li>
+                        <li>Cilantro</li>
+                        <li>Chili Powder</li>
+                        <li>Cumin</li>
+                        <li>Salt</li>
+                        <li>Pepper</li>
+                        <li>Corn tortillas</li>
+                    </ul>
+                </div>
+                <div class="directions">
+                    <h3>Directions</h3>
+                    <ol>
+                        <li>Cut and mix: Cut the flank steak into one inch pieces. In a medium sized bowl add the soy sauce, Worcestershire, juice from one lime, cilantro, chili powder, cumin, and salt and pepper.</li>
+                        <li>Marinate: Add the steak and let marinate in the fridge for 1-2 hours.</li>
+                        <li>Cook the meat: Heat a medium sized skillet to medium high heat. Add the steak and marinade and cook for about 5-7 minutes or until no longer pink.</li>
+                        <li>Add toppings: Put the steak in the center of your tortilla with desired toppings.</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="side-dishes-container">
+                <h3>Need some side dishes?</h3>
+                <div class="side-dishes-flex">
+                    <div>
+                        <img src="images/mexican-rice.png" alt="mexican rice">
+                        <div class="food-description-flex">
+                            <h4>Mexican Rice</h4>
+                            <p>Traditional recipe with a twist.</p>
+                            <span>
+                                Rating: 
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="images/salsa.png" alt="salsa food">
+                        <div class="food-description-flex">
+                            <h4>Salsa</h4>
+                            <p>Just the right amount of spice.</p>
+                            <span>
+                                Rating: 
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="images/guacamole.png" alt="guacamole food">
+                        <div class="food-description-flex">
+                            <h4>Guacamole</h4>
+                            <p>Fresh and healthy.</p>
+                            <span>
+                                Rating: 
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="images/tortillas.png" alt="tortillas food">
+                        <div class="food-description-flex">
+                            <h4>Tortillas</h4>
+                            <p>Easy to make and so good.</p>
+                            <span>
+                                Rating: 
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                            </span>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <nav>
+                <label for="dropdown" id="label-dropdown">
+                    <!--hamburger-->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" style="enable-background:new 0 0 40 40" xml:space="preserve">
+                        <path d="M36 17.6c.1 1.8.1 3.6.1 5.4.1 2.7 0 5.5-.9 8-.5 1.4-1.3 2.6-2.4 3.6-3.3 2.7-6 2.4-12.6 2.4-4.6 0-9.2.2-12.2-1.9-2.5-1.8-3.6-4.2-3.8-7.1-.2-3.6.1-7.2.1-10.8 0-2.6.6-5.1 1.9-7.4 2.5-4.5 7.3-7.5 12.4-8 .4-.1.9-.1 1.4-.1 5.2 0 10.1 2.6 13 6.8 1 1.5 1.8 3.1 2.3 4.9.4 1.3.6 2.7.7 4.2z" style="fill:none;stroke:#eb5428;stroke-width:2;stroke-miterlimit:10"/>
+                        <path d="M36.1 22.6c0 .5-.1 1-.5 1.4-.5.7-1.4 1.2-2.6 1.3-2.8.2-5.8.1-8.6.1h-12c-1.3 0-2.6 0-3.8-.1-1.1 0-2.4 0-3.3-.6-.8-.6-1.2-1.4-1.2-2.2 0-.9.3-1.8 1.5-2.3.9-.4 2-.5 3.1-.5 1.4 0 2.8-.1 4.3-.1h14.5c1.2 0 2.3 0 3.5.1 1.7.1 3.3-.1 4.4 1 .3.6.7 1.3.7 1.9z" style="fill:none;stroke:#fff;stroke-width:2.0832;stroke-miterlimit:10"/>
+                        <path d="M38.5 22.6c0 .9-.2 1.7-.5 2.5-.6 1.3-1.7 2.1-3 2.2-3.3.3-6.7.2-9.9.2H11.2c-1.5 0-3-.1-4.4-.1-1.3-.1-2.8 0-3.8-1-.9-1-1.3-2.5-1.3-3.8 0-1.6.3-3.1 1.7-4 1-.7 2.3-.8 3.5-.8 1.6-.1 3.3-.1 4.9-.1H28.5c1.3 0 2.7 0 4 .1 2 .1 3.8-.1 5.1 1.7.5.9.9 2 .9 3.1z" style="fill:none;stroke:#eb5428;stroke-width:2.9408;stroke-miterlimit:10"/>
+                    </svg> 
+                </label>
+                <input type="checkbox" id="input-dropdown" />
+                <ul class="nav-icons">
+                    <li class="nav1" id="nav1">
+                        <a href="">
+                            <!--logo-->
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-tools-kitchen" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M4 3h8l-1 9h-6z" />
+                                <path d="M7 18h2v3h-2z" />
+                                <path d="M20 3v12h-5c-.023 -3.681 .184 -7.406 5 -12z" />
+                                <path d="M20 15v6h-1v-3" />
+                                <line x1="8" y1="12" x2="8" y2="18" />
+                            </svg> -->
+                            <img src="images/logo.PNG" alt="logo">
+                        </a>
+                    </li>
+                    <li><a href=""><img src="images/breakfast.png" alt="breakfast"></a><p>Breakfast</p></li>
+                    <li><a href=""><img src="images/brunch.png" alt="brunch"></a><p>Brunch</p></li>
+                    <li><a href=""><img src="images/lunch.png" alt="lunch"></a><p>lunch</p></li>
+                    <li><a href=""><img src="images/dinner.png" alt="dinner"></a><p>Dinner</p></li>
+                    <li><a href=""><img src="images/appetizers.png" alt="appetizers"></a><p>Appetizers</p></li>
+                    <li><a href=""><img src="images/desserts.png" alt="desserts"></a><p>Desserts</p></li>
+                    <li><a href=""><img src="images/beverages.png" alt="beverages"></a><p>Beverages</p></li>
+                    <li class="nav-last" id="nav-last">
+                        <a href="">
+                            <!--avatar-->
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <circle cx="12" cy="12" r="9" />
+                                <circle cx="12" cy="10" r="3" />
+                                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                            </svg> -->
+                            <img src="images/avatar.PNG" alt="avatar">
+                        </a>
+                        <p class="sign-in">Sign in</p>
+                    </li>
+                </ul>
+            </nav>
+            <div class="search">
+                <input type="search" name="search" id="search-input" placeholder="find a recipe">
+                <!--search-->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFFFFF" fill="#EB5428" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="10" cy="10" r="7" fill="#EB5428"/>
+                    <line x1="21" y1="21" x2="15" y2="15" />
+                </svg>
+            </div>
+            <footer>
+                <div class="footer-icons-div">
+                    <ul class="footer-icons">
+                        <li>
+                            <!--facebook-->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                            <p>facebook</p>
+                        </li>
+                        <li>
+                            <!--instagram-->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                            <p id="p2">instagram</p>
+                        </li>
+                        <li>
+                            <!--youtube-->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-youtube" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="3" y="5" width="18" height="14" rx="4" />
+                                <path d="M10 9l5 3l-5 3z" />
+                            </svg>
+                            <p>youtube</p>
+                        </li>
+                        <li>
+                            <!--tiktok-->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-tiktok" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#EB5428" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M9 12a4 4 0 1 0 4 4v-12a5 5 0 0 0 5 5" />
+                            </svg>
+                            <p id="p4">tiktok</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <h4>Copyright 2022 <br>Kyle Emmanuel</h4>
+                    <img src="images/taco-cheesington.png" alt="taco cheesington">
+                </div>
+            </footer>
+        </main>
+    </div>
+</body>
+</html>
